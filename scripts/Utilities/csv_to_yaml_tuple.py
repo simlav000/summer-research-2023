f69698872e7e510d59a81ad6fef3d0291139b2c3
Simon Lavoie <132480613+simlav000@users.noreply.github.com>
2023-08-17 12:53:14 -0400

    Update csv_to_yaml_tuple.py to process single file or directory. Generalized.

--- scripts/Utilities/csv_to_yaml_tuple.py
+++ scripts/Utilities/csv_to_yaml_tuple.py
@@ -1,72 +1,85 @@
-import csv
 import os
-import yaml
+import numpy as np
 import pandas as pd
 
-"""
-This script is designed specifically to read wavelength and index of refraction data from a CSV file
-and format it into yaml for use in chroma.
+'''
+This script is meant to take in as input the path to a directory holding files which contain
+wavelength-dependent optical properties. It outputs a file with said optical properties formatted
+in a manner readable by chroma.
 
-Usage: Run the script from the command line. The script will prompt the user to input a directory holding
-CSV files. It will then prompt the user to input a filename for which the script will write to. The CSV
-files should contain a header, and have two or three columns:
-wavelength (wl), refraction coefficient (n), and extinction coefficient (k). 
+The CSV's should be formatted according to the following two supported header styles:
 
-The output of the script looks something like:
+1: wl,property,,"source"
 
-Material1:
-  IndexOfRefractionRe:
-    0: !!python/tuple [wl0,n0]
-    1: !!python/tuple [wl1,n1]
-    ...
-  IndexOfRefractionIm:
-    0: !!python/tuple [wl0,k0]
-    1: !!python/tuple [wl1,k1]
-    ...
-Material2:
-    ...
+    where wl is the wavelength (in nm), the properties include (but are not limited to):
+      - Detection
+      - IndexOfRefractionRe
+      - ScatteringLength (in mm) <- ALL LENGTHS BUT WAVELENGTHS SHOULD BE mm
+      - ...
+    and the source is written between quotes. It is optional
     
-"Material1" and "Material2" are determined automatically by the filename of the CSV. If the CSV does not contain
-a third column, only IndexOfRefractionRe will be written to the file.
-"""
+2: wl,n,k,"source"
+
+    where wl is the wavelength (in nm), n and k are the real and imaginary parts of the refractive 
+    index, respectively. The source is optional.
+
+Ensure the data has the correct units
+
+'''
+
+def sanitize_key(key):
+    # Remove whitespaces and special characters to form valid YAML keys
+    # Ideally, the user sticks to naming their property in the CSV to whatever the YAML
+    # recognizes, for instance: AbsorptionLength, SpecularReflectivity, IndexOfRefractionRe, etc.
+    # If this is not done, at least we have this.
+    return "".join(c if c.isalnum() else "" for c in key)
+
 
 def csv_to_yaml(csv_file, yaml_file):
-    # get filename without path or extension
-    file_name = os.path.splitext(os.path.basename(csv_file))[0] 
+    file_name = os.path.splitext(os.path.basename(csv_file))[0]
 
-    data_frame  = pd.read_csv(csv_file)
+    data_frame = pd.read_csv(csv_file)
 
-    # extract data
-    wavelengths           = data_frame.iloc[:, 0].values
-    refractive_index_real = data_frame.iloc[:, 1].values
+    header = data_frame.columns.tolist()
+    prop_name = sanitize_key(header[1])  # Use the second column header as property name
+    source = header[-1] if len(header) > 3 else None  # Source optional
 
-    # create key for each csv file analyzed
+    wavelengths = data_frame.iloc[:, 0].values
+    property_values = data_frame.iloc[:, 1].values
+
+
+    if source:
+        yaml_file.write(f"# source: {source}\n")
+
     yaml_file.write(f"{file_name}:\n")
-    yaml_file.write(f"  IndexOfRefractionRe:\n")
+
+    if prop_name.lower() in ["n", "indexofrefractionre"]:
+        yaml_file.write(f"  IndexOfRefractionRe:\n")
+    else:
+        yaml_file.write(f"  {prop_name}:\n")
 
     for i in range(len(wavelengths)):
-        yaml_file.write(f"    {i}: !!python/tuple [{wavelengths[i]},{refractive_index_real[i]}]\n")
+        # Format wavelength and property value with proper precision (e.g., scientific notation)
+        yaml_file.write(f"    {i}: !!python/tuple [{wavelengths[i]:.6e}, {property_values[i]:.6e}]\n")
 
-    if len(data_frame.columns) > 2:
+    if len(header) > 2:
         # Extract the third column values if they exist
         refractive_index_imaginary = data_frame.iloc[:, 2].values
-        yaml_file.write(f"  IndexOfRefractionIm:\n")
-        for i in range(len(wavelengths)):
-            yaml_file.write(f"    {i}: !!python/tuple [{wavelengths[i]},{refractive_index_imaginary[i]}]\n")
-        
+        if any(np.isnan(np.array(refractive_index_imaginary))):
+            pass # deals with case where source is provided increasing length of header
+                 # but no data is present at the third column
+        else:   
+            yaml_file.write(f"  IndexOfRefractionIm:\n")
+            for i in range(len(wavelengths)):
+                yaml_file.write(f"    {i}: !!python/tuple [{wavelengths[i]:.6e}, {refractive_index_imaginary[i]:.6e}]\n")
 
-        
 
 # Prompt the user for the folder path containing CSV files
 folder_path = input("Enter the folder path containing CSV files: ")
-yaml_file_name = input("Enter the output YAML file name: ")
+yaml_file_name = input("Enter the output YAML file path/name: ")
 
-yaml_file = open(yaml_file_name, 'w')
-
-# Iterate through all CSV files in the folder and convert them to YAML
-for file in os.listdir(folder_path):
-    if file.endswith(".csv"):
-        csv_file = os.path.join(folder_path, file) # full file path
-        csv_to_yaml(csv_file, yaml_file)
-
-
+with open(yaml_file_name, 'w') as yaml_file:
+    for file in os.listdir(folder_path):
+        if file.endswith(".csv"):
+            csv_file = os.path.join(folder_path, file)  # full file path
+            csv_to_yaml(csv_file, yaml_file)
